876859abba6194c99f4af65bc8a952fb33da1726
tanshiyun <2031347096@qq.com>
2019-12-28 22:56:30 +0800

    初始提交

---
+++ src/main/java/com/base/foundation/verify/token/TokenImageCreator.java
@@ -0,0 +1,136 @@
+package com.base.foundation.verify.token;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Random;
+
+/**
+ * @ClassName TokenImageCreator
+ * @Description 图形验证码生成类
+ * @Author tsy20
+ * @Date 2019/12/28
+ **/
+public class TokenImageCreator {
+
+    private int width; //图片宽度
+    private int height; //图片高度
+    private int fontSize; //验证码字体大小
+    private String backgroundColor; //图片背景色
+
+    private int red;
+    private int green;
+    private int blue;
+
+    private Random random = new Random();
+
+    //字体数组
+    private String[] fontFamily = {"Lucida Calligraphy", "Consolas", "Bell MT", "Ink Free", "Segoe Print"};
+    //字体样式数组
+    private int[] fontStyle = {Font.ITALIC+Font.BOLD, Font.PLAIN, Font.ITALIC, Font.BOLD};
+
+
+    public TokenImageCreator() {
+        setWidth(160);
+        setHeight(70);
+        setFontSize(this.height/2);
+        setBackgroundColor("230,230,250");
+    }
+
+
+    /**
+     * 绘制验证码图片
+     * @param content 图片内容
+     * @return 返回图片字节数组
+     */
+    public byte[] createImage(String content) {
+        BufferedImage image = new BufferedImage(this.width, this.height, 1);
+
+        Graphics2D graphics = image.createGraphics();
+
+        //绘制背景
+        graphics.setColor(new Color(this.red, this.green, this.blue));
+        graphics.fillRect(0, 0, this.width, this.height);
+
+        //计算字符绘制坐标
+        int baseX = (this.width % content.length())/2;
+        int baseY = (this.height - this.fontSize)/2 + this.fontSize;
+
+        int step = this.width / content.length();
+
+        //绘制验证码字符
+        for(int i=0; i<content.length(); i++){
+            //设置随机颜色和字体
+            graphics.setColor(getRandomColor());
+            graphics.setFont(getRandomFont());
+
+            graphics.drawString(String.valueOf(content.charAt(i)), baseX+i*step, baseY);
+        }
+
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+        try {
+            ImageIO.write(image, "png", out);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                out.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return out.toByteArray();
+
+
+    }
+
+
+
+    /**
+     * 返回随机颜色
+     * @return color
+     */
+    public Color getRandomColor(){
+        int red = (100-this.red)+random.nextInt(155);
+        int green = (100-this.green)+random.nextInt(155);
+        int blue = (100-this.blue)+random.nextInt(155);
+        return new Color(red,green,blue);
+    }
+
+
+
+    /**
+     * 返回随机字体
+     * @return font
+     */
+    public Font getRandomFont(){
+        return new Font(fontFamily[random.nextInt(fontFamily.length)], fontStyle[random.nextInt(fontStyle.length)], fontSize);
+    }
+
+
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public void setFontSize(int fontSize) {
+        this.fontSize = fontSize;
+    }
+
+    public void setBackgroundColor(String backgroundColor) {
+        this.backgroundColor = backgroundColor;
+        String[] colorParam = backgroundColor.split(",");
+
+        this.red = Integer.parseInt(colorParam[0]);
+        this.green = Integer.parseInt(colorParam[1]);
+        this.blue = Integer.parseInt(colorParam[2]);
+    }
+}
---
+++ src/main/java/com/base/foundation/verify/token/TokenManager.java
@@ -0,0 +1,201 @@
+package com.base.foundation.verify.token;
+
+import java.util.Random;
+
+/**
+ * @ClassName TokenManager
+ * @Description token管理类
+ * @Author tsy20
+ * @Date 2019/12/28
+ **/
+public class TokenManager {
+
+    private int tokenLength; //随机码长度
+    private boolean ignoreCase; //是否忽略大小写标志
+    private Random random = new Random();
+
+    public TokenManager() {
+        this.tokenLength = 6; //token默认长度设置为6
+        this.ignoreCase = true; //默认校验时忽略大小写
+    }
+
+    /**
+     * 根据默认长度生成随机大小写字母序列
+     * @return sequence
+     */
+    public String generateRandomAlphabet(){
+
+        StringBuilder sequence = new StringBuilder();
+
+        for(int i=0; i<this.tokenLength; i++){
+
+            //大小写随机标识
+            int caseFlag = random.nextInt(2);
+
+            if(caseFlag > 0){
+                sequence.append(getRandomLower());
+            }else {
+                sequence.append(getRandomUpper());
+            }
+        }
+
+        return sequence.toString();
+    }
+
+
+
+    /**
+     * 生成指定长度随机大小写字母序列
+     * @return sequence
+     */
+    public String generateRandomAlphabet(int length){
+
+        StringBuilder sequence = new StringBuilder();
+
+        for(int i=0; i<length; i++){
+
+            //大小写随机标识
+            int caseFlag = random.nextInt(2);
+
+            if(caseFlag > 0){
+                sequence.append(getRandomLower());
+            }else {
+                sequence.append(getRandomUpper());
+            }
+        }
+
+        return sequence.toString();
+    }
+
+
+    /**
+     * 生成默认长度数字序列
+     */
+    public String generateRandomNum(){
+
+        StringBuilder sequence = new StringBuilder();
+
+        for (int i = 0; i < this.tokenLength; i++) {
+            sequence.append(getRandomNum());
+        }
+
+        return sequence.toString();
+    }
+
+
+
+    /**
+     * 生成指定长度数字序列
+     */
+    public String generateRandomNum(int length){
+
+        StringBuilder sequence = new StringBuilder();
+
+        for (int i = 0; i < length; i++) {
+            sequence.append(getRandomNum());
+        }
+
+        return sequence.toString();
+    }
+
+
+
+    /**
+     * 生成默认长度大小写字母+数字混合序列
+     */
+    public String generateMultiSeq(){
+        StringBuilder sequence = new StringBuilder();
+
+        for (int i = 0; i < this.tokenLength; i++) {
+            getRandomCharacter(sequence);
+        }
+
+        return sequence.toString();
+    }
+
+
+    /**
+     * 生成指定长度大小写字母+数字混合序列
+     */
+    public String generateMultiSeq(int length){
+        StringBuilder sequence = new StringBuilder();
+
+        for (int i = 0; i < length; i++) {
+            getRandomCharacter(sequence);
+        }
+
+        return sequence.toString();
+    }
+
+
+    /**
+     * 随机生成大小写字母或数字
+     * @param sequence
+     */
+    private void getRandomCharacter(StringBuilder sequence) {
+
+        int typeFlag = random.nextInt(2);
+        if (1>typeFlag){
+            sequence.append(getRandomNum());
+        }else{
+            int caseFlag = random.nextInt(2);
+            if(1>caseFlag){
+                sequence.append(getRandomUpper());
+            }else{
+                sequence.append(getRandomLower());
+            }
+        }
+    }
+
+
+    /**
+     * 返回随机0-9整数
+     * @return
+     */
+    public String getRandomNum(){
+        return String.valueOf(random.nextInt(10));
+    }
+
+
+
+    /**
+     * 返回随机大写字母
+     * @return
+     */
+    public String getRandomUpper(){
+        char u = (char) (random.nextInt(26)+65); //大写字母ascii范围65-90
+        return String.valueOf(u);
+    }
+
+
+
+    /**
+     * 返回随机小写字母
+     * @return
+     */
+    public String getRandomLower(){
+        char l = (char) (random.nextInt(26)+97); //小写字母ascii范围97-122
+        return String.valueOf(l);
+    }
+    
+
+
+
+
+    public int getTokenLength() {
+        return tokenLength;
+    }
+
+    public void setTokenLength(int tokenLength) {
+        this.tokenLength = tokenLength;
+    }
+
+    public boolean isIgnoreCase() {
+        return ignoreCase;
+    }
+
+    public void setIgnoreCase(boolean ignoreCase) {
+        this.ignoreCase = ignoreCase;
+    }
+
+}
